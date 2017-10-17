390876e85de5720474a4a07683869d136bba86af
Oscar Chujutalli <oscarchs.94@gmail.com>
2017-10-17 07:59:09 -0500

    comparación de tiempos odd-even

---
+++ openmp/matrix_vector.cpp
@@ -0,0 +1,9 @@
+#include <omp.h>
+#include <iostream>
+using namespace std;
+#define size 100
+
+
+int main(){
+    return 0;
+}
--- openmp/oddeven_openmp.cpp
+++ openmp/oddeven_openmp.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <time.h>
 using namespace std;
-
 
 void oddEvenSort(int a[],int n){
 	int phase,i,temp;
-    int thread_count = 2;
+    int thread_count = 4;
 #   pragma omp parallel num_threads(thread_count) default(none) shared(a,n) private(i,temp,phase)
 	for( phase=0 ; phase<n ; phase++){
 		if(phase%2 == 0){
@@ -31,14 +31,52 @@ void oddEvenSort(int a[],int n){
 		}
 	}
 
-	for(int i=0;i<10;i++){
-        printf("%d, ", a[i]);
+}
+
+
+void oddEvenSort1(int a[],int n){
+	int phase,i,temp;
+    int thread_count = 4;
+	for( phase=0 ; phase<n ; phase++){
+		if(phase%2 == 0){
+#   		pragma omp parallel for num_threads(thread_count) default(none) shared(a,n) private(i,temp)
+			for(i=1;i<n;i+=2){
+				if(a[i-1]>a[i]){
+					temp = a[i];
+					a[i] = a[i-1];
+					a[i-1]=temp;
+				}
+			}
+		}
+		else{
+#   		pragma omp parallel for num_threads(thread_count) default(none) shared(a,n) private(i,temp)
+			for( i=1 ; i<n-1 ; i+=2){
+				if(a[i]>a[i+1]){
+					temp=a[i];
+					a[i]=a[i+1];
+					a[i+1]=temp;
+				}
+			}
+		}
 	}
+
 }
 
 
 int main(){
-	int lista[10]={5,2,3,0,9,1,4,6,7,8};
-	oddEvenSort(lista,10);
+	int size = 100000;
+	int lista[size];
+	for(int i =0; i < size ; i++){
+		srand(time(NULL));
+		lista[i] = rand()%10000;
+	}
+
+	double start = omp_get_wtime();
+	oddEvenSort(lista,size);
+	printf("Time: \t %f \n", omp_get_wtime()-start);
+
+	start = omp_get_wtime();
+	oddEvenSort1(lista,size);
+	printf("Time: \t %f \n", omp_get_wtime()-start);
 	return 0;
 }
